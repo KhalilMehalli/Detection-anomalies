0ddda1b1888abe852649cce0934a43e9e4a9d4c8
KhalilMehalli <khalilmehalli@gmail.com>
2025-07-15 13:44:27 +0200

    Parser added in benchmark.py

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,14 +1,22 @@
 import os
 import time
+import sys
+import argparse
 from datetime import datetime
+
 import numpy as np
 import pandas as pd
+
 from sklearn.metrics import roc_curve, auc, recall_score, roc_auc_score
 from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import Bunch
 
+from pyod.models.vae import VAE
+from pyod.models.deep_svdd import DeepSVDD
+from pyod.models.ae1svm import AE1SVM
+from pyod.models.knn import KNN
 from pyod.models.auto_encoder import AutoEncoder
 
         
@@ -78,6 +86,9 @@ def run_iforest(bunch, params):
     auc = roc_auc_score(y_ground_truth, scores)
     recall = recall_score(y_ground_truth, y_pred)
 
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+
     return {
         'auc': auc,
         'recall': recall,
@@ -124,6 +135,9 @@ def run_lof(bunch, params) :
     auc = roc_auc_score(y_ground_truth, scores)
     recall = recall_score(y_ground_truth, y_pred)
 
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
     return {
         'auc': auc,
         'recall': recall,
@@ -166,12 +180,123 @@ def run_autoencoder(bunch, params):
     auc = roc_auc_score(y_ground_truth, scores)
     recall = recall_score(y_ground_truth, y_pred)
     
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
     return {
         'auc':    auc,
         'recall': recall,
         'time':   elapsed
     }
 
+def run_vae(bunch, params):
+    x, y_ground_truth = bunch.data, bunch.target
+    VAE_model = VAE(**params)
+
+    # Fit + predict 
+    start = time.perf_counter()
+    VAE_model.fit(x)
+    y_pred = VAE_model.predict(x)
+    elapsed = time.perf_counter() - start
+
+    # metrics
+    scores = VAE_model.decision_function(x)
+    
+    auc = roc_auc_score(y_ground_truth, scores)
+    recall = recall_score(y_ground_truth, y_pred)
+
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
+    return {
+        'auc':    auc,
+        'recall': recall,
+        'time':   elapsed
+    }
+
+
+
+def run_deepsvdd(bunch, params):
+    x, y_ground_truth = bunch.data, bunch.target
+
+    # DeepSVDD have a mandatory parameters, the features numbers of the dataset      <- <- <-
+    features = x.shape[1]
+    params["n_features"] = features
+    
+    DS = DeepSVDD(**params)
+
+    # Fit + predict 
+    start = time.perf_counter()
+    DS.fit(x)
+    y_pred = DS.predict(x)
+    elapsed = time.perf_counter() - start
+
+    # metrics 
+    scores = DS.decision_function(x)
+    
+    auc = roc_auc_score(y_ground_truth, scores)
+    recall = recall_score(y_ground_truth, y_pred)
+
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
+    return {
+        'auc':    auc,
+        'recall': recall,
+        'time':   elapsed
+    }
+
+def run_ae1svm(bunch, params):
+    x, y_ground_truth = bunch.data, bunch.target
+
+    AE1SVM_model = AE1SVM(**params)
+
+    # Fit + predict 
+    start = time.perf_counter()
+    AE1SVM_model.fit(x)
+    y_pred = AE1SVM_model.predict(x)
+    elapsed = time.perf_counter() - start
+
+    # metrics
+    scores = AE1SVM_model.decision_function(x)
+    
+    auc = roc_auc_score(y_ground_truth, scores)
+    recall = recall_score(y_ground_truth, y_pred)
+
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
+    return {
+        'auc':    auc,
+        'recall': recall,
+        'time':   elapsed
+    }
+
+def run_knn(bunch, params):
+    x, y_ground_truth = bunch.data, bunch.target
+    KNN_model = KNN(**params)
+
+    # Fit + predict 
+    start = time.perf_counter()
+    KNN_model.fit(x)
+    y_pred = KNN_model.predict(x)
+    elapsed = time.perf_counter() - start
+
+    # metrics
+    scores = KNN_model.decision_function(x)
+    
+    auc = roc_auc_score(y_ground_truth, scores)
+    recall = recall_score(y_ground_truth, y_pred)
+
+    total_time = time.perf_counter() - start
+    print("Total execution time", total_time)
+    
+    return {
+        'auc':    auc,
+        'recall': recall,
+        'time':   elapsed
+    }
+    
 def benchmark(datasets, algorithms):
     """
     Run a benchmark of multiple algorithms on multiple datasets.
@@ -327,6 +452,8 @@ def automatisation(datasets, algorithms, filepath = "./latex_results.txt"):
 
     """
     print("DÉBUT")
+    start = time.perf_counter()
+
     # Calculate all the metrics
     df_auc, df_recall, df_time = benchmark(datasets, algorithms)
 
@@ -335,34 +462,120 @@ def automatisation(datasets, algorithms, filepath = "./latex_results.txt"):
 
     # Write these latex tab in a file
     write_latex_in_file(latex_auc, latex_recall,latex_time, algorithms, filepath)
-    print("FIN")
+    elapsed = time.perf_counter() - start
+    
+    print("FIN apres ", elapsed, "secondes !!!")
 
 
+def load_datasets(names=None):
+    """
+    Load only the requested datasets into memory as lists of unch objects.
+
+    Parameters
+    ----------
+    names : list of str or None
+        If provided, contains the base filenames (without .csv) to load.
+        If None, all CSV files are loaded.
+
+    Returns
+    -------
+    list of bunch
+        Each bunch has attributes .name, .data, .target.
+    """
+    
+    all_files = list_all_file_name_in_folder()
+    if names:
+        # Keep only the datasets whose  appears in the provided names list.
+        selected_files = [f for f in all_files 
+                          if os.path.splitext(os.path.basename(f))[0] in names ]
+    else:
+        # Or select all datasets
+        selected_files = all_files
+
+    print("Datasets used :", selected_files)
+    # Convert the selected csv files into a list of bunch objects.
+    return csv_data_into_bunch(selected_files)
+
+def get_algorithms(selected=None):
+    """
+    Selected only the algorithms the user want.
+
+    Parameters
+    ----------
+    selected : list of str or None
+        If provided, contains the algorithm names 
+        to include. If None, all supported algorithms are returned.
+        
+    Returns
+    -------
+    list of tuples
+        Each tuple is (str, function, dict).
+    """
+    
+    all_algos = [
+        ("IForest",     run_iforest,     {"random_state":10}),
+        ("LOF",         run_lof,         {}),
+        ("AE", run_autoencoder, {"random_state":10, "preprocessing": False,"epoch_num": 10}),
+        ("VAE",         run_vae,         {"random_state":10, "preprocessing": False,"epoch_num": 10}),
+        ("DeepSVDD",    run_deepsvdd,    {"random_state":10,"preprocessing": False,"epochs": 10}),
+        ("AE1SVM",      run_ae1svm,      {"preprocessing": False,"epochs": 10}),
+        ("KNN",         run_knn,         {})
+    ]
+
+    if selected:
+        # Filter the full list to only include those is in the provided selected list 
+        sel = {name.lower() for name in selected}
+        selected_algo = [t for t in all_algos 
+                if t[0].lower() in sel]
+        print("Algortihms used :", sel)
+        return selected_algo
+
+    print("All algorithms used")
+    return all_algos
+
+
+def parse_args():
+    """
+    Parse command-line arguments to select datasets and algorithms.
+
+    Returns
+    -------
+    Namespace
+        Attributes:
+        - dataset: list of chosen dataset names (or None)
+        - algo:    list of chosen algorithm names (or None)
+    """
+    
+    ds_names = [os.path.splitext(os.path.basename(p))[0] for p in list_all_file_name_in_folder()]
+    algo_names = ['iforest','lof','ae','vae','deepsvdd','ae1svm','knn']
+    
+    parser = argparse.ArgumentParser(description="Benchmark script for multiple anomaly detection algorithms")
+
+    choices = [n.lower() for n in ds_names]
+                                
+    parser.add_argument(
+        "-d",
+        nargs="+", # 1 or multiple datasets
+        metavar=(choices,"Other dataset if you want"),
+        choices=choices, # choices possible 
+        type=str.lower, # convert the user in lowercase 
+        help="Dataset(s) to load (use base filename, without .csv). Default: all."
+    )
+    parser.add_argument(
+        "-a",
+        nargs="+",
+        metavar=(algo_names,"Other algo if you want"),
+        choices=algo_names,
+        type=str.lower,
+        help="Algorithm(s) to run. Default: all."
+    )
+    return parser.parse_args()
+    
 if __name__ == "__main__":
-    filenames = list_all_file_name_in_folder()
     
-    datasets = csv_data_into_bunch(filenames)
-    
-    """
-    algorithms = [
-     ("IForest", run_iforest, {'n_estimators':200, 'contamination':0.1}),
-     ("LOF", run_lof, {'n_neighbors':300, 'contamination':0.1}),
-     ("AutoEncoder", run_autoencoder, {'contamination':0.1})
-    ]
-    """
-    """
-    algorithms = [
-     ("IForest", run_iforest, {'n_estimators':100, 'contamination':0.1}),
-     ("AutoEncoder", run_autoencoder, {'contamination':0.1})
-    ]
-    """
-    
-    algorithms = [
-     ("IForest", run_iforest, {'n_estimators':100, 'contamination':0.1}),
-     ("LOF", run_lof, {'n_neighbors':150, 'contamination':0.1}),
-     ("AutoEncoder", run_autoencoder, {'contamination':0.1})
-    ]
-    
-    
+    args = parse_args()
+    datasets = load_datasets(args.d)
+    algorithms = get_algorithms(args.a)
+
 
     automatisation(datasets, algorithms)
